a60aba32a2116f9be63a0bcdf5c64ede2ed5db05
kmatheus <kmathews.blaze@gmail.com>
2024-07-25 13:26:45 -0300

    Add module internet-speed-test

---
+++ internet-speed-test/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SpeedTest from './SpeedTest';
+
+function App() {
+  return (
+    <div className="App">
+      <SpeedTest />
+    </div>
+  );
+}
+
+export default App;
---
+++ internet-speed-test/src/SpeedTest.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+const SpeedTest = () => {
+  const [speedData, setSpeedData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const measureSpeed = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('/speedtest');
+      if (!response.ok) {
+        throw new Error('Erro ao medir a velocidade da internet');
+      }
+      const data = await response.json();
+      setSpeedData(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>KimeratechServices - Teste de Velocidade (Internet)</h1>
+      <button onClick={measureSpeed} disabled={loading}>
+        {loading ? 'Medindo...' : 'Medir Velocidade'}
+      </button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {speedData && (
+        <div>
+          <p>Velocidade de Download: {speedData.download_speed.toFixed(2)} Mbps</p>
+          <p>Velocidade de Upload: {speedData.upload_speed.toFixed(2)} Mbps</p>
+          <p>Ping: {speedData.ping} ms</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SpeedTest;
